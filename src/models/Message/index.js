9a776d44e0d35c1864a33751caf038d7975e1833
程宝生 <chenbaosheng@build4dream.com>
2018-08-16 16:31:26 +0800

    聊天模块

--- src/models/Message/index.js
+++ src/models/Message/index.js
@@ -5,12 +5,17 @@
 * @Last Modified time: 2018-06-06 18:41:18
 */
 import React, { Component } from 'react'
-import { Header, Content } from 'Components'
+import { Tabs } from 'antd-mobile'
+import { Content } from 'Components'
 import NewIcon from 'Components/NewIcon'
 import * as urls from 'Contants/urls'
 import history from 'Util/history'
 import style from './style.css'
 
+const tabs = [
+  { title: '消息' },
+  { title: '待处理' }
+]
 class Message extends Component {
   constructor(props) {
     super(props)
@@ -26,31 +31,52 @@ class Message extends Component {
   }
   render() {
     return (
-      <div className='contentBox'>
-        <Header title='消息'/>
-        <Content>
-          <div onClick={this.handleSysNotice} className={`${style['notice-box']} my-bottom-border`}>
-            <dl>
-              <dt>
-                <span>
-                  <NewIcon className={style['notice-icon']} type='icon-xiaolaba' />
-                </span>
-              </dt>
-              <dd>
-                <p>消息通知<em>下午4：20</em></p>
-                <span>您直接在线接单就可以了。</span>
-              </dd>
-            </dl>
-          </div>
-          <ul className={style['mesg-list']}>
-            <li className='my-bottom-border' onClick={this.handleMesage}>
-              <div className={`${style['usr-header']} my-full-border`}>小明</div>
-              <div className={style['msg-box']}>
-                <p>江西华夏建筑有限公司<em>下午 2:23 </em></p>
-                <span>您直接在线接单就可以了。</span>
+      <div className={`${'contentBox'} ${style['message-content']}`}>
+        <Content isHeader={false}>
+          <Tabs tabs={tabs}
+            initalPage={'t1'}
+            tabBarBackgroundColor='#FAFAFA'
+          >
+            <div>
+              <div onClick={this.handleSysNotice} className={`${style['notice-box']} my-bottom-border`}>
+                <dl>
+                  <dt>
+                    <span>
+                      <NewIcon className={style['notice-icon']} type='icon-xiaolaba' />
+                    </span>
+                  </dt>
+                  <dd>
+                    <p>消息通知<em>下午4：20</em></p>
+                    <span>您直接在线接单就可以了。</span>
+                  </dd>
+                </dl>
               </div>
-            </li>
-          </ul>
+              <ul className={style['mesg-list']}>
+                <li className='my-bottom-border' onClick={this.handleMesage}>
+                  <div className={`${style['usr-header']} my-full-border`}>小明</div>
+                  <div className={style['msg-box']}>
+                    <p>江西华夏建筑有限公司<em>下午 2:23 </em></p>
+                    <span>您直接在线接单就可以了。</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <div onClick={this.handleSysNotice} className={`${style['notice-box']} my-bottom-border`}>
+                <dl>
+                  <dt>
+                    <span>
+                      <NewIcon className={style['notice-icon']} type='icon-xiaolaba' />
+                    </span>
+                  </dt>
+                  <dd>
+                    <p>消息通知<em>下午4：20</em></p>
+                    <span>您直接在线接单就可以了。</span>
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </Tabs>
         </Content>
       </div>
     )
