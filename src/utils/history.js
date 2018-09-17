c638d245301302187f225435c66ec332e547b9c5
程宝生 <chenbaosheng@build4dream.com>
2018-09-17 10:07:56 +0800

    history

--- src/utils/history.js
+++ src/utils/history.js
@@ -4,6 +4,6 @@
 * @Last Modified by:   baosheng
 * @Last Modified time: 2018-04-04 14:44:33
 */
-import createHashHistory from 'history/createHashHistory'
+import createBrowserHistory from 'history/createBrowserHistory'
 
-export default createHashHistory()
+export default createBrowserHistory()
