9994011434201a33950e7105bcd46e25f8d460be
程宝生 <chenbaosheng@build4dream.com>
2018-12-04 16:03:10 +0800

    add jpush

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,8 @@ module.exports = {
     "PRE": true,
     "AMap": true,
     "AMapUI": true,
-    "OCBridge": true
+    "OCBridge": true,
+    "device": true
   },
 
   // http://eslint.org/docs/rules/xxx, xxx代表rule名称, 0=off, 1=warning, 2=error
--- src/contants/tooler.js
+++ src/contants/tooler.js
@@ -6,8 +6,8 @@
 */
 import arrayTreeFilter from 'array-tree-filter'
 import history from 'Util/history'
-
 const DATE_REGEXP = new RegExp('(\\d{4})-(\\d{2})-(\\d{2})([T\\s](\\d{2}):(\\d{2}):(\\d{2})(\\.(\\d{3}))?)?.*')
+
 export const getSel = (value, optionsObj) => { // 根据键值筛选数结果数据中的对象，value:需要筛选树的value数据，optionsObj: 所在的树的数据
   if (!value) {
     return ''
@@ -36,6 +36,7 @@ export const returnFloat = (number) => { // 金额加小数点，保留2位
 
 export const getQueryString = (name) => { // url转json
   let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
+  // let r = window.location.search.substr(1).match(reg)
   let r = history.location.search.substr(1).match(reg)
   if (r !== null) {
     return r[2]
@@ -48,17 +49,17 @@ export const parseJsonUrl = (ojson) => { // json对象转URL
   let name = null
   let key = null
   for (let p in ojson) {
-    if (typeof ojson[p] === undefined) { return null }
+    if (!ojson[p]) { return null }
     if (ojson.hasOwnProperty(p)) { name = p }
     key = ojson[p]
-    s += '&' + name + '=' + encodeURIComponent(key)
+    s += '&' + name + '=' + encodeURIComponent(key.toString())
   }
   return s.substring(1, s.length)
 }
 
 export const parseURLParam = (url) => { // 把url的参数部分转化成json对象
   try {
-    let targeturl = url || window.location.href
+    let targeturl = url || (history.location.pathname + history.location.search)
     const regUrl = /^[^\?]+\?([\w\W]+)$/
     const regPara = /([^&=]+)=([\w\W]*?)(&|$|#)/g
     let arrUrl = regUrl.exec(targeturl)
@@ -78,10 +79,10 @@ export const parseURLParam = (url) => { // 把url的参数部分转化成json对
 }
 
 /*
-			大数字每3位添加逗号
-			@method addCommas
-			@param {Number} number 需要转换的数字
-		*/
+      大数字每3位添加逗号
+      @method addCommas
+      @param {Number} number 需要转换的数字
+    */
 export const addCommas = (number) => {
   let newStr = ''
   let count = 0
@@ -125,6 +126,39 @@ export const formatDate = function (date) {
   return y + '-' + m + '-' + d
 }
 
+export const queryTourl = function (backquery) {
+  /*
+    &backquery=a:1,b:1,c:1
+  */
+  let queryAry = backquery.split(',')
+  let queryStr = ''
+  queryAry.map((item) => {
+    queryStr += item.split(':')[0] + '=' + item.split(':')[1] + '&'
+  })
+  queryStr = '?' + queryStr.slice(0, queryStr.length - 1)
+  return queryStr
+}
+
+export const getPosition = (timeout = 10000, maximumAge = 60000, enableHighAcuracy = false) => new Promise((resolve, reject) => {
+  if (!navigator && !navigator.geolocation) {
+    return reject(new Error('geolocation api not supported'))
+  }
+  const success = (loc) => {
+    const location = {
+      latitude: loc.coords.latitude, // 纬度
+      longitude: loc.coords.longitude, // 经度
+      accuracy: loc.coords.accuracy // 精确度
+    }
+    resolve(location)
+  }
+  const error = () => reject('出错了')
+  navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAcuracy, // 指示浏览器获取高精度的位置，默认为false
+    timeout, // 指定获取地理位置的超时时间，默认不限时，单位为毫秒
+    maximumAge // 最长有效期，在重复获取地理位置时，此参数指定多久再次获取位置。
+  })
+})
+
 export const stringToDate = (dateString) => { // 字符串转Date类型
   if (DATE_REGEXP.test(dateString)) {
     let timestamp = dateString.replace(DATE_REGEXP, function($all, $year, $month, $day, $part1, $hour, $minute, $second, $part2, $milliscond) {
@@ -136,4 +170,50 @@ export const stringToDate = (dateString) => { // 字符串转Date类型
     return date
   }
   return null
+}
+
+export const onBackKeyDown = function (e) {
+  let backUrl = history.location.pathname.split('/')
+  if (
+    (backUrl[1] === 'Home' && backUrl.length <= 2) ||
+    (backUrl[1] === 'ReceiveOrder' && backUrl.length <= 2) ||
+    (backUrl[1] === 'Mine' && backUrl.length <= 2) ||
+    (backUrl[1] === 'Message' && backUrl.length <= 2) || backUrl[1] === ''
+  ) {
+    showToast('再点击一次退出!', 2000)
+    document.removeEventListener('backbutton', onBackKeyDown, false) // 注销返回键
+    document.addEventListener('backbutton', exitApp, false) // 绑定退出事件
+    // 2秒后重新注册
+    var intervalID = window.setInterval(function () {
+      window.clearInterval(intervalID)
+      document.removeEventListener('backbutton', exitApp, false) // 注销返回键
+      document.addEventListener('backbutton', onBackKeyDown, false) // 返回键
+    }, 2000)
+  } else {
+    history.goBack()
+  }
+}
+var exitApp = function () {
+  navigator.app.exitApp()
+}
+
+export const showToast = function (msg, duration) {
+  duration = isNaN(duration) ? 3000 : duration
+  var m = document.createElement('div')
+  m.innerHTML = msg
+  m.style.cssText = 'width:60%; min-width:150px; background:#000;opacity: 0.8; height:40px; color:#fff; line-height:40px; text-align:center; border-radius:5px; position:fixed; top:70%; left:20%; z-index:999999; font-weight:bold;'
+  document.body.appendChild(m)
+  setTimeout(function () {
+    var d = 0.8
+    m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in'
+    m.style.opacity = '0'
+    setTimeout(function () {
+      document.body.removeChild(m)
+    }, d * 1000)
+  }, duration)
+}
+export const backButton = function () {
+  document.addEventListener('deviceready', () => {
+    document.addEventListener('backbutton', onBackKeyDown)
+  })
 }
