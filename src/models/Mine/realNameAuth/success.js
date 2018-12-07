cf397a8f7d12456df240687e6a82971f1ddf3515
白鹤 <baihe@yaque365.com>
2018-12-07 20:13:31 +0800

    update

--- src/models/Mine/realNameAuth/success.js
+++ src/models/Mine/realNameAuth/success.js
@@ -1,19 +1,31 @@
 
 import React, { Component } from 'react'
 import { Icon } from 'antd-mobile'
-// import * as urls from 'Contants/urls'
+import * as urls from 'Contants/urls'
 import { Header, Content } from 'Components'
 import style from './style.css'
+import { onBackKeyDown } from 'Contants/tooler'
 class RealNameAuth extends Component {
+  componentDidMount () {
+    document.removeEventListener('backbutton', onBackKeyDown, false)
+    document.addEventListener('backbutton', this.backButtons, false)
+  }
+  backButtons = (e) => {
+    this.props.match.history.push(urls['HOME'])
+  }
+  componentWillUnmount () {
+    document.removeEventListener('backbutton', this.backButtons)
+    document.addEventListener('backbutton', onBackKeyDown, false)
+  }
   render() {
     return <div className='pageBox'>
       <Header
         title={'认证成功'}
         leftIcon='icon-back'
-        leftTitle1='返回'
+        leftTitle1='返回首页'
         leftClick1={() => {
           // this.props.match.history.push(urls['REALNAMEAUTH'])
-          this.props.match.history.go(-1)
+          this.props.match.history.push(urls['HOME'])
         }}
       />
       <Content>
