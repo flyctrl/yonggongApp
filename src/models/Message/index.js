94f559da6ce2ccc453a433f660153a842b394e44
白鹤 <baihe@yaque365.com>
2018-12-21 10:28:31 +0800

    update

--- src/models/Message/index.js
+++ src/models/Message/index.js
@@ -62,7 +62,7 @@ class Message extends Component {
     return await data['list'] || []
   }
   componentDidMount() {
-    const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop - 140
+    const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop - 148.5
     this.genData().then((rdata) => {
       this.rData = rdata
       this.setState({
