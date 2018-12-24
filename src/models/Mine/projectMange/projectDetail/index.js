a067b02545ce3545c5a7e7015baa7f6465c5a764
白鹤 <baihe@yaque365.com>
2018-12-24 11:30:52 +0800

    项目详情

--- src/models/Mine/projectMange/projectDetail/index.js
+++ src/models/Mine/projectMange/projectDetail/index.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
 import { List } from 'antd-mobile'
 import { Header, Content } from 'Components'
-// import NewIcon from 'Components/NewIcon'
+import NewIcon from 'Components/NewIcon'
 // import * as urls from 'Contants/urls'
 import * as tooler from 'Contants/tooler'
 import api from 'Util/api'
 // import { projectStatus } from 'Contants/fieldmodel'
 import style from './style.css'
-import noticeicon from 'Src/assets/home/noticeicon.png'
 const Item = List.Item
-// const prjStatus = {
-//   1: '待开工',
-//   2: '施工中',
-//   3: '已作废',
-//   4: '已完成'
-// }
-// const isShowPrj = {
-//   0: '不显示',
-//   1: '显示'
-// }
+const prjStatus = {
+  1: '待开工',
+  2: '施工中',
+  3: '已作废',
+  4: '已完成'
+}
+const projectStatus = {
+  1: '审核中',
+  2: '已审核',
+  3: '审核未通过'
+}
+const isShowPrj = {
+  0: '不显示',
+  1: '显示'
+}
 class ProjectDetail extends Component {
   constructor(props) {
     super(props)
@@ -27,7 +31,7 @@ class ProjectDetail extends Component {
       dataSource: {},
       isLoading: true,
       tabIndex: tooler.getQueryString('tabIndex'),
-      url: tooler.getQueryString('url')
+      coordinate: '{}'
     }
   }
   getProjectDetail = async () => {
@@ -36,12 +40,12 @@ class ProjectDetail extends Component {
     const data = await api.Mine.projectMange.projectDetail({
       prj_no: id
     }) || false
-    // console.log(data)
     if (data) {
       this.setState({
         dataSource: data,
         fileList: data['attachment'],
-        isLoading: false
+        isLoading: false,
+        coordinate: data['coordinate']
       })
     }
   }
@@ -49,6 +53,7 @@ class ProjectDetail extends Component {
     this.getProjectDetail()
   }
   render() {
+    let { dataSource = {}, fileList = [], coordinate = '{}' } = this.state
     return (
       <div className='pageBox gray'>
         <Header
@@ -60,80 +65,82 @@ class ProjectDetail extends Component {
           }}
         />
         <Content>
-          <div className={style['detail-box']}>
-            <div className={style['detail-prj']}>
-              <List renderHeader={() => '项目信息'}>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'九江市修水县木青华商住区一期8栋 拷贝'}>项目名称</Item>
+          {
+            JSON.stringify(dataSource) !== '{}'
+              ? <div className={style['detail-box']}>
+                <div className={style['detail-prj']}>
+                  <List renderHeader={() => '项目信息'}>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_name']}>项目名称</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_no']}>项目编号</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={projectStatus[dataSource['status']]}>项目审核状态</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={prjStatus[dataSource['bid_status']]}>项目状态</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={isShowPrj[dataSource['is_show']]}>项目是否显示</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_win_bid_unit']}>项目中标单位</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_win_bid_notice_no']}>中标通知书编号</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['created_at']}>创建时间</Item>
+                    </div>
+                  </List>
                 </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'无'}>项目编号</Item>
+                <div className={style['detail-work']}>
+                  <List renderHeader={() => '项目施工信息'}>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['licence_no']}>施工许可证编号</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_construct_unit']}>施工单位</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['prj_construct_unit_code']}>施工单位统一社 会信用代码</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['construction_amount']}>项目施工金额</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={`${dataSource['construction_area']}平方米`}>项目施工面积</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={dataSource['construction_place']}>项目施工地址</Item>
+                    </div>
+                    <div className={style['input-ellipsis']}>
+                      <Item extra={<span>{`经度:${JSON.parse(coordinate)['lng']}`}<br/>{`纬度:${JSON.parse(coordinate)['lat']}`}</span>}>项目施工坐标</Item>
+                    </div>
+                  </List>
                 </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'已审核'}>项目审核状态</Item>
+                <div className={style['detail-about']}>
+                  <List renderHeader={() => '项目概括'}>
+                    <div className={`${style['input-detail-about']}`}>
+                      {dataSource['prj_brief']}
+                    </div>
+                    <ul className={`${style['input-detail-fujian']} my-top-border`}>
+                      <span>附件</span>
+                      {
+                        fileList.map((item, index) => {
+                          return (
+                            <li key={index} className='my-bottom-border'><NewIcon type='icon-paperclip' className={style['file-list-icon']}/><a>{item.org_name}</a></li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </List>
                 </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'待开工'}>项目状态</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'显示'}>项目是否显示</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'浙江亚雀科技有限公司'}>项目中标单位</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'45126325582'}>中标通知书编号</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'2018-09-09  18:45'}>创建时间</Item>
-                </div>
-              </List>
-            </div>
-            <div className={style['detail-work']}>
-              <List renderHeader={() => '项目施工信息'}>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'45126325582'}>施工许可证编号</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'无'}>施工单位</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'154125151514'}>施工单位统一社 会信用代码</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'369.00'}>项目施工金额</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'2656'}>项目施工面积</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={'浙江亚雀科技有限公司很长的地址 很长很长 拷贝'}>项目施工地址</Item>
-                </div>
-                <div className={style['input-ellipsis']}>
-                  <Item extra={<span>{'经度:120.00000000'}<br/>{'经度:120.00000000'}</span>}>项目施工坐标</Item>
-                </div>
-              </List>
-            </div>
-            <div className={style['detail-about']}>
-              <List renderHeader={() => '项目概括'}>
-                <div className={`${style['input-detail-about']}`}>
-                    水电费健康上岛咖啡借款收到两份借款收到甲方扩大开放接口水电费觉得水电费健康上岛咖啡借款收到两份借款收
-                    到甲方扩大开放接水电费健康上岛咖啡借款收到两份借款收到甲方扩大
-                    开放接口水电费觉得口水电费觉得水电费健康上岛咖啡借款收到两份借款收到甲方扩大开放接口水电费觉得
-                    水电费健康上岛咖啡借款收到两份借款收到甲方扩大开放接口水电费觉得
-                    水电费健康上岛咖啡借款收到两份借款收到甲方扩大开放接口水电费觉得
-                </div>
-                <div className={`${style['input-detail-fujian']} my-top-border`}>
-                  <span>附件</span>
-                  <div className={style['detail-img']}>
-                    <img src= {noticeicon}/>
-                    <img src= {noticeicon}/>
-                    <img src= {noticeicon}/>
-                  </div>
-                </div>
-              </List>
-            </div>
-          </div>
+              </div>
+              : null
+          }
         </Content>
       </div>
     )
